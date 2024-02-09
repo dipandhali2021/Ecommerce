8812d0d6bed752875440018dab78d5d7ddf17f01
dipandhali2021 <dipandhali2021@gmail.com>
2024-02-09 16:29:41 +0530

    vite config changed

--- ecommerce_frontend/vite.config.ts
+++ ecommerce_frontend/vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy:{
-      '/ecommerce-assets':{
-        target: 'https://res.cloudinary.com/da9skd1ks/image/upload/v1706444003',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    proxy: {
+      "/ecommerce-assets":
+        "https://res.cloudinary.com/da9skd1ks/image/upload/v1706444003",
+    },
   },
-  
-  
-})
+});
